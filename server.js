6993141fe661759628371abfd78112c98f27a1e9
WingChhun <capt.wing.chhun@gmail.com>
2018-06-02 20:17:57 -0700

    Add hgeroku script ready for deploymenit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const PORT = process.env.PORT || 5000; //* Port
 const dev = (process.env.NODE_ENV === 'production');
 
 //*Routes
